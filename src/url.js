3181dd427ddd1ad7ae05dda4c72090000f6d9a82
ammar <iluffy120@gmail.com>
2024-08-18 17:25:26 +0300

    forgot to change the api url

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-// const API_URL = 'https://pinterest-api-3ttx.onrender.com'
-const API_URL = 'http://localhost:55'
+const API_URL = 'https://pinterest-api-3ttx.onrender.com'
+// const API_URL = 'http://localhost:55'
 export default API_URL 
